57f3208b1a894c06627a0b400afc488175cec07b
Dave Copeland <davetron5000@gmail.com>
2010-12-12 12:38:51 -0500

    test refactor

--- test/tc_todolist.rb
+++ test/tc_todolist.rb
@@ -9,37 +9,35 @@ class TC_testTodoList < Test::Unit::TestCase
   include FileUtils
   include Hacer
 
+  def setup
+    FakeFS.activate!
+    @filename = '/tmp/todo'
+    rm_f(@filename)
+  end
+
+  def teardown
+    FakeFS.deactivate!
+  end
+
 
   def test_create_new_todolist
-    FakeFS do
-      filename = "/tmp/todo"
-      rm(filename)
-      assert !File.exists?(filename),"Expected #{filename} not to exist"
-      todo_list = Todolist.new(filename)
-      assert File.exists?(filename),"Expected todolist file #{filename} to have been created"
-    end
+    assert !File.exists?(@filename),"Expected #{@filename} not to exist"
+    todo_list = Todolist.new(@filename)
+    assert File.exists?(@filename),"Expected todolist file #{@filename} to have been created"
   end
 
   def test_use_existing_todolist
-    FakeFS do
-      filename = "/tmp/todo"
-      rm(filename)
-      File.open(filename,'w') { |file| YAML.dump(["FOO"],file) }
-      expected_contents = YAML.dump(["FOO"])
-      todo_list = Todolist.new(filename)
-      contents = File.readlines(filename).join("\n") + "\n"
-      assert_equal expected_contents,contents,"#{filename} got overwritten when it shouldn't have been"
-    end
+    File.open(@filename,'w') { |file| YAML.dump(["FOO"],file) }
+    expected_contents = YAML.dump(["FOO"])
+    todo_list = Todolist.new(@filename)
+    contents = File.readlines(@filename).join("\n") + "\n"
+    assert_equal expected_contents,contents,"#{@filename} got overwritten when it shouldn't have been"
   end
 
-  def test_bad_format_causes_exception
-    FakeFS do
-      filename = "/tmp/todo"
-      rm(filename)
-      File.open(filename,'w') { |file| file.puts "FOO" }
-      assert_raises ArgumentError do 
-        todo_list = Todolist.new(filename)
-      end
+  def test_bad_form_fat_causes_exception
+    File.open(@filename,'w') { |file| file.puts "FOO" }
+    assert_raises ArgumentError do 
+      todo_list = Todolist.new(@filename)
     end
   end
 end
